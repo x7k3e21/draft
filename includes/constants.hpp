5cc4d37d3344337be52c2cd860e274d32b73a1ea
x7k3e21 <uly4nkin.denis@gmail.com>
2023-07-04 22:38:18 +0300

    CMake building script configured

---
+++ includes/constants.hpp
@@ -0,0 +1,8 @@
+
+#ifndef DRAFT_CONSTANTS_H
+#define DRAFT_CONSTANTS_H
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+#endif
---
+++ sources/engine.cpp
@@ -0,0 +1,9 @@
+
+#include <stdint.h>
+
+#include "constants.hpp"
+
+int32_t main(int32_t argc, char *argv[])
+{
+    return EXIT_SUCCESS;
+}
